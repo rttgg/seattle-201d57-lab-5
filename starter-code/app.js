0025b1d0ad6acb9f2570b8d170e256e032f3855d
Roman Gebrehiwot <rttgg1@gmail.com>
2019-06-09 21:13:17 -0700

    adding and fix problem 4

--- starter-code/app.js
+++ starter-code/app.js
@@ -93,12 +93,16 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+    var addTwoArray = sum(2,3);
+    var addThreeArray = sum(addTwoArray[0],4);
+    var totalArray = testArray + ' was passed in as an array of numbers, and ' + 
+                    addThreeArray[0] + ' is their sum.'
+    return [addThreeArray[0], totalArray];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
